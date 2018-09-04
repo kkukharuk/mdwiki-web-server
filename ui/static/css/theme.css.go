75b95b8b58dbd9e3a5d456413bbd5e27a7c89415
Mister87 <Konstantin@Kukharuk.ru>
2018-09-04 13:43:02 +0300

    Added logging in ui.static.css

--- ui/static/css/theme.css.go
+++ ui/static/css/theme.css.go
@@ -2,12 +2,11 @@ package css
 
 import (
 	"encoding/base64"
-	"log"
 	"net/http"
 )
 
-func ThemeCSS(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Getting page: %s", r.URL.Path)
+func (cfg *Config) ThemeCSS(w http.ResponseWriter, r *http.Request) {
+	cfg.Logger.Debugf("Getting page: %s", r.URL.Path)
 	css_b64 := "Ym9keSB7CiAgcGFkZGluZy10b3A6IDcwcHg7CiAgcGFkZGluZy1ib3R0b206IDMwcHg7Cn0KCi50aGVtZS1kcm9wZG93biAuZHJvcGRvd24tbWVudSB7CiAgcG9zaXRpb246IHN0YXRpYzsKICBkaXNwbGF5OiBibG9jazsKICBtYXJnaW4tYm90dG9tOiAyMHB4Owp9CgoudGhlbWUtc2hvd2Nhc2UgPiBwID4gLmJ0biB7CiAgbWFyZ2luOiA1cHggMDsKfQoKLnRoZW1lLXNob3djYXNlIC5uYXZiYXIgLmNvbnRhaW5lciB7CiAgd2lkdGg6IGF1dG87Cn0K"
 	css, _ := base64.StdEncoding.DecodeString(css_b64)
 	w.Header().Set("Content-Type", "text/css")
