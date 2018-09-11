5849c0e16a35010ecbdb96f24ae1e007e226923e
Mister87 <Konstantin@Kukharuk.ru>
2018-09-11 05:07:41 +0300

    Update: added getting pid function

--- deamon/deamon.go
+++ deamon/deamon.go
@@ -17,6 +17,7 @@ type Config struct {
 }
 
 func (cfg *Config) Run(mdPath string) error {
+	//cfg.createPid()
 	cfg.Logger.Infof("Create listener: %s:%d", cfg.Host, cfg.Port)
 	listenSpec := fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)
 	l, err := net.Listen("tcp", listenSpec)
@@ -38,4 +39,16 @@ func (cfg *Config) waitForSignal(app ui.Config) error {
 	s := <-ch
 	cfg.Logger.Infof("Got signal: %v, exiting.", s)
 	return app.Stop()
+}
+
+func (cfg *Config) createPid() {
+	pid, err := os.OpenFile("mdwiki-web-server.pid", os.O_WRONLY|os.O_CREATE, 0600)
+	defer pid.Close()
+	if err != nil {
+		cfg.Logger.Errorf("Error create pid-file: %v", err)
+	}
+	_, err = pid.WriteString(fmt.Sprintf("%d", os.Getgid()))
+	if err != nil {
+		cfg.Logger.Errorf("Error write pid number in pid-file: %v", err)
+	}
 }
